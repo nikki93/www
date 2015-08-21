200191aba5f008b4121807f80298a6eb527faeef
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-08-21 03:00:15 -0700

    fix site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://princeton.edu/~sigatapu'
+SITEURL = 'http://www.nikhilesh.info'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
